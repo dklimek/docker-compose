7c9c1bfbeadf6adb15d286c793ed4fda93a46612
KomputerMisia <klimekd1@o2.pl>
2021-02-02 19:06:15 +0100

    #Fix - Start the spring project

---
+++ src/main/java/pl/klimek/springbootapidocker/SpringbootApiDockerApplication.java
@@ -0,0 +1,13 @@
+package pl.klimek.springbootapidocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApiDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApiDockerApplication.class, args);
+	}
+
+}
